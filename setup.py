de8b289c18ee58a60ad390b8c589976371e2ab14
Sourav Sinha <Phoenixdelta74>
2025-02-15 10:18:38 +0530

    Changes related Packages done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages , setup
+
+
+setup(
+
+    name = "Gen AI Project",
+    version = '0.0.0.0',
+    author = "Sourav Sinha",
+    author_email = "sinhasourav74@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
